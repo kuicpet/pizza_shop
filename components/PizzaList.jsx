cc6fc7757e3c51cde9fe41cfb90a8332d3b2dbd5
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-01-05 04:03:36 +0100

    ft--Product list and Product card added

---
+++ components/PizzaList.jsx
@@ -0,0 +1,23 @@
+import styles from '../styles/PizzaList.module.css'
+import PizzaCard from './PizzaCard'
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>Lorem picsus enter hhet uusg ngsht best pizza in toenmshh hhsjjsnh nsyushbdvsgub n asysajab</p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  )
+}
+
+export default PizzaList
