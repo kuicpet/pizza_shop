97a380709aabfdbc6459df47757b9fee914311a8
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2022-01-05 14:15:57 +0100

    ft--footer added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,45 @@
+import Image from 'next/image'
+import styles from '../styles/Footer.module.css'
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <Image src='/img/bg.png' alt='' layout='fill' objectFit='cover' />
+            </div>
+            <div className={styles.item}>
+                <div className={styles.card}>
+                    <h2 className={styles.motto}>OH YES, WE BAKED SLICE OF PIZZA</h2>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>FIND OUR PIZZA SHOPS</h1>
+                    <p className={styles.text}>
+                        No 52 Don Road, #304
+                        <br/> New York, 85023
+                        <br/> (604) 875-7884
+                    </p>
+                    <p className={styles.text}>
+                        No 52 Don Road, #304
+                        <br/> New York, 85023
+                        <br/> (604) 875-7884
+                    </p>
+                    <p className={styles.text}>
+                        No 52 Don Road, #304
+                        <br/> New York, 85023
+                        <br/> (604) 875-7884
+                    </p>
+                    <p className={styles.text}>
+                        No 52 Don Road, #304
+                        <br/> New York, 85023
+                        <br/> (604) 875-7884
+                    </p>
+                </div>
+                <div className={styles.card}>
+                    <h1 className={styles.title}>WORKING HOURS</h1>
+                    <p>MONDAY - FRIDAY <br/>9:00 - 22: 00</p>
+                    <p>SATURDAY - SUNDAY <br/>12:00 - 24: 00</p>
+                </div>
+            </div>
         </div>
     )
 }
--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -4,7 +4,7 @@ import PizzaCard from './PizzaCard'
 const PizzaList = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>BEST PIZZA IN TOWN</h1>
+      <h1 className={styles.title}>Best Pizza In Town</h1>
       <p className={styles.desc}>Lorem picsus enter hhet uusg ngsht best pizza in toenmshh hhsjjsnh nsyushbdvsgub n asysajab</p>
       <div className={styles.wrapper}>
         <PizzaCard />
